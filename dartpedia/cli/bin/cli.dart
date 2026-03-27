f2e3737007494176ba8d758748db582c0eabe0ab
nznislove <gger111@yandex.ru>
2026-03-28 01:42:41 +0300

    first commit

---
+++ dartpedia/cli/bin/cli.dart
@@ -0,0 +1,21 @@
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    print('Search command recognized!');
+  } else {
+    printUsage();
+  }
+}
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
+

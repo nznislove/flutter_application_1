dfcc5af72096cf065b002bc741a1d3d0886769f2
nznislove <gger111@yandex.ru>
2026-03-28 01:53:03 +0300

    добавлен поиск

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,3 +1,4 @@
+import 'dart:io'; // Add this line at the top
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -7,11 +8,32 @@ void main(List<String> arguments) {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
     // Add this new block:
-    print('Search command recognized!');
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
 }
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into the CLI into a single string
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+
 void printUsage() { // Add this new function
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
